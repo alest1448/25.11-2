4518d431b7f066b15978fa40e5854e6e9c088e8c
alest1448 <118421044+alest1448@users.noreply.github.com>
2022-11-25 16:43:36 +0900

    Initial commit

---
+++ 25.11-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._11_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: "); 
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            
+            int result;
+            if (n > 123)
+                result = Math.Abs((n - 123) * 3); 
+            else
+                result = Math.Abs(123 - n); 
+            Console.WriteLine(result);
+        }
+    }
+}
